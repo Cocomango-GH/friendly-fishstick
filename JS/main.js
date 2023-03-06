47c1af1a7c85d3be16757f4ab5ed30b3bdebe563
cocomango-GH <sashaamah@gmail.com>
2023-03-05 22:08:40 -0800

    added css

--- JS/main.js
+++ JS/main.js
@@ -1,7 +1,35 @@
-console.log("hello world")
+
 /*----- constants -----*/
+//memorycards
+
+const memorycards = {
+
+}
+
 /*----- app's state (variables) -----*/
+// flip over
+// cards
+// hold the board 
+
+
+
 /*----- cached element references -----*/
 
+// flip over
+// cards
+// hold the board 
+
+
 /*----- event listeners -----*/
+// A event listener that listen for clicks for the start button and cards//
+
+
+
 /*----- functions -----*/
+//when game starts show board for 15 seconds then filip over///
+// show wrong guess for 2 seconds flip back over//
+// when guess correctly leave card flipped over//
+/// after three wrong guess end game //
+
+//shuffle deck after game player loses and restart game//
+
--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # friendly-fishstick
+Welcome to the game of Friendly-FishSticks can you help the fishstick find their firends.
+find the matching fishsticks. 
+
+Rules of the game 
+Player will have 15 seconds to look over cards before they turn over. 
+the player will have three chances to find the right match or the player loses the game. How many fish sticks will you find. 
