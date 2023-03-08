3b537c3aa07e545286d2e53ee6842b72d213e337
cocomango-GH <111953271+Cocomango-GH@users.noreply.github.com>
2023-03-08 14:04:36 -0800

    matched is working

--- JS/main.js
+++ JS/main.js
@@ -1,70 +1,75 @@
-console.log('im working')
 /*----- constants -----*/
-//const audio = new Audio();//
-//memorycards//
-const memoryCards = document.querySelectorAll('.cards')
-//give cards a value//
+
 const myCards = [
-    {id: 1, value: 'card1'},
-    {id: 2, value: 'card2'},
-    {id: 3, value: 'card3'},
-    {id: 4, value: 'card4'},
+  {id: 1, value: 'card1'},
+  {id: 1, value: 'card1'},
+  {id: 2, value: 'card2'},
+  {id: 2, value: 'card2'},
 ]
 
-
-
 /*----- app's state (variables) -----*/ 
+let matches = 0;
 let flipped = [];
-let scores = 0;// 10 pts for every match//
-let matches = 0;// // if player gets three wrong guesses games over//
-let winner = false;// matches all cards win the game//
-
-
+let card
+/*----- cached elements  -----*/
+const memoryCards = document.querySelectorAll('.cards')
 
 /*----- event listeners -----*/
-// A event listener that listen for clicks for the start button and cards//
 memoryCards.forEach(card => {
- card.addEventListener('click', clickedCards)
+  card.addEventListener('click', clickedCards)
 });
 
 /*----- functions -----*/
-//cards now have eventlistener when clicked//
+
 function clickedCards(evt) {
     const memCard = evt.target;
-// cards flipped //
-  if(flipped.includes(memCard)) {
-    return;
-  }
-// add card to the array //
-flipped.push(memCard);
-// add card flip over//
-memCard.classList.add('flipped');
-//check if cards flip//
-if (flipped.length === 2) {
-    checkForMatch();
- }
+    // memCard.classList.add('flipped');
+    flipped.push(memCard)
+    matchCards();
+    console.log('iclicked you')
 }
-function checkForMatch() {
-    const firstCard = flipped[0];
-    const secondCard = flipped[1];
 
-if(firstCard.dataset.id === secondCard.dataset.id) {
-    firstCard.classList.add('matched');
-    secondCard.classList.add('matched');
-    scores += 10;
-    matches++;
-    if(matches === myCards.length / 2) {
-        winner = true;
-    }else{
-        setTimeout(() => {
-            firstCard.classList.remove('flipped');
-            secondCard.classList.remove('flipped');
-        } , 1000);
-    }
-    flipped =[];
-}
-}
-// check if card has been flipped 
+function matchCards() {
+// i need two cards
+   if(flipped.length === 2) {
+    if(flipped[0].classList[1] === flipped[1].classList[1]){
+console.log('matched')
+    } else (flipped[0].classList[2] != flipped[0].classList[1])
+    console.log('unmatched')
 
-/// cards need to have afcuntion for matching cards when clicked//
+}}
+// function clickedCards(evt) {
+//   const memCard = evt.target;
 
+//   if (flipped.includes(memCard)) {
+//     return;
+//   }
+ 
+//   flipped.push(memCard);
+//   memCard.classList.add('flipped');
+
+//   if (flipped.length === 2) {
+//     checkForMatch();
+//   }
+// }
+
+// function checkForMatch() {
+//   const firstCard = flipped[0];
+//   const secondCard = flipped[1];
+
+//   if (firstCard.dataset.id === secondCard.dataset.id) {
+//     firstCard.classList.add('matched');
+//     secondCard.classList.add('matched');
+//     matches++;
+
+//     if (matches === myCards.length / 2) {
+//       // All cards have been matched
+//       console.log('You win!');
+//    } else {
+//      setTimeout(() => {
+//         firstCard.classList.remove('flipped');
+//      secondCard.classList.remove('flipped');
+//     }, 1000);
+//    }
+
+//      flipped = []; 
